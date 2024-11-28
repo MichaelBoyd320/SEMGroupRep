a2940a4a742950d45051e48664401eb1e53151b4
40646479 <40646479@live.napier.ac.uk>
2024-11-28 17:47:08 +0000

    added country class, and get country and display country

--- src/main/java/com/napier/sem/PopRep.java
+++ src/main/java/com/napier/sem/PopRep.java
@@ -1,4 +1,53 @@
 package com.napier.sem;
 
-public class PopRep {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PopRep
+{
+    private Connection con;
+
+    public PopRep(Connection conget)
+    {
+        con = conget;
+    }
+    public ArrayList<Country> getCountriesPop()
+    {
+        // creating an initial empty arraylist
+        ArrayList countriesPop = new ArrayList();
+
+        String getCountires = "SELECT Code, Name, Continent, Population FROM countries ORDER BY population DESC";
+
+        try
+        {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(getCountires);
+
+            while (rs.next())
+            {
+                Country country = new Country();
+                country.code = rs.getString("Code");
+                country.name = rs.getString("Name");
+                country.continent = rs.getString("Continent");
+                country.population = rs.getInt("Population");
+                countriesPop.add(country);
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
+
+
+        return countriesPop;
+    }
+    public void printCountires(ArrayList<Country> countries)
+    {
+        for (Country country : countries)
+        {
+            System.out.println(country.code + " " + country.name + " " + country.continent + " " + country.population);
+        }
+    }
 }
