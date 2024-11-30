52083335b8feeef74093ac4bdbf7311849c2376e
michael <40646479@live.napier.ac.uk>
2024-11-30 17:58:37 +0000

    countryPoprep improving sort

--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -7,5 +7,5 @@ public class Country
     public String continent;
     public int population;
 
-
+    public int getPopulation() {return population;}
 }
--- src/main/java/com/napier/sem/PopRep.java
+++ src/main/java/com/napier/sem/PopRep.java
@@ -2,6 +2,7 @@ package com.napier.sem;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -16,21 +17,29 @@ public class PopRep
     public ArrayList<Country> getCountriesPop() {
         // creating an initial empty arraylist
         ArrayList countriesPop = new ArrayList();
-
+        // query for getting the pops in order
         String getCountires = "SELECT Code, Name, Continent, Population FROM country ORDER BY population DESC";
 
-
-        try (PreparedStatement pstmt = con.prepareStatement(getCountires)) {
+        // a try catch just incase something goes wrong
+        // also prepares the statement for execution
+        try (PreparedStatement pstmt = con.prepareStatement(getCountires))
+        {
+            // makes a result set
             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
+                // goes through the result set and adds each rows details to a new country object
                 Country country = new Country();
                 country.code = rs.getString("Code");
                 country.name = rs.getString("Name");
                 country.continent = rs.getString("Continent");
                 country.population = rs.getInt("Population");
+
+                // adds the country object to the list
                 countriesPop.add(country);
             }
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
+            // shows what went wrong, if there was an issue
             System.out.println(e.toString());
         }
 
@@ -39,12 +48,17 @@ public class PopRep
     }
     public void printCountires(ArrayList<Country> countries, StringBuilder output)
     {
+        countries.sort(Comparator.comparing(Country::getPopulation));
+
+        // prints a title for this part
         System.out.println("Printing Countries");
         for (Country country : countries)
         {
-            output.append("Country: " + country.code).append("Population: " + country.population).append("\n");
+            // adds all the parts of the list to a output
+            output.append("Country: " + country.code).append(" Population: " + country.population).append("\n");
 
         }
+        // prints the output
         System.out.println(output.toString());
     }
 }
